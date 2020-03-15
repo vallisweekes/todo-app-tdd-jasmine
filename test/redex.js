39576392bac94dbbf70ffd2cc24507ab3ad18a9e
vallisweekes <vallis.weekes@gmail.com>
2020-03-15 22:00:48 +0000

    code update

--- test/redex.js
+++
@@ -1,8 +0,0 @@
-{
-    "type": 'ADD_NAME',
-    "payload": 'Vallis'
-}
-
-const reducer = (prevState, action)=> {
-    (action.type === 'ADD_NAME') ? [...prevState, something.action] : prevState;
-} 
